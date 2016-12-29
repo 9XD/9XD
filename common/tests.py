7c15df33ea658f2c811ec9ceb9298611171086d8
Hyeon Mook Jerry Choi <chm073@gmail.com>
2016-12-29 22:59:54 +0900

    Add get home test.

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from test_plus.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    def test_get_home(self):
+        home_url = self.reverse('home')
+        self.get_check_200(home_url)
+        self.assertResponseContains('<title>9XD</title>')
+
