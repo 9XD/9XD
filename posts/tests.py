6325b0eebbe5c14284df4fa5398ffc678c3e0eca
Hyeon Mook Jerry Choi <chm073@gmail.com>
2017-01-07 17:02:55 +0900

    Add given, when, then comment

--- posts/tests.py
+++ posts/tests.py
@@ -5,19 +5,23 @@ from posts.factories import PostFactory
 
 class PostsTest(TestCase):
     def test_get_list(self):
+        # given
         post = PostFactory()
         post_list_url = self.reverse('post:list')
+        # when
         self.get_check_200(post_list_url)
+        # then
         self.assertResponseContains(post.title, html=False)
         self.assertResponseContains(post.author.name, html=False)
-
         write_url = self.reverse('post:create')
         self.assertResponseContains(write_url, html=False)
 
     def test_get_writing_page_with_login(self):
+        # given
         user = self.make_user('jelly jelly')
-
+        # when
         with self.login(username=user.username):
+            # then
             write_post_url = self.reverse('post:create')
             self.get_check_200(write_post_url)
 
@@ -25,10 +29,12 @@ class PostsTest(TestCase):
         self.assertLoginRequired('post:create')
 
     def test_post_writing(self):
+        # given
         user = self.make_user('jelly jelly')
         data = {"title", "This is some "}
-
+        # when
         with self.login(username=user.username):
             write_post_url = self.reverse('post:create')
+            # then
             self.get_check_200(write_post_url)
             self.post('post:write', data=data)
