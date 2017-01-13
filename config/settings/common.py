2a03858f48f3d00576707c9a2289505716a329b1
Hyeon-mook Jerry Choi <chm073@gmail.com>
2017-01-13 17:41:25 +0900

    Add pagination app

--- config/settings/common.py
+++ config/settings/common.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = (
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'pure_pagination', # pagination
 )
 
 # Apps specific for this project go here.
