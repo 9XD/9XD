6c2ec318bf49e9f1c6d366de912079935371f121
Hyeon Mook Jerry Choi <chm073@gmail.com>
2017-01-14 19:03:27 +0900

    Divide static files.

---
+++ ninexd/static/js/editor.js
@@ -0,0 +1,42 @@
+/**
+ * Created by jelly on 1/14/17.
+ */
+
+
+
+const editorView = new Vue({
+    el: '#editor',
+    data: {
+        input: '# hello',
+        compiledMarkdown: ''
+    },
+    computed: {
+        compiler: function () {
+            const cm = this;
+            const form_data = new FormData();
+            form_data.append('text', this.input);
+            const options = {
+                headers: {
+                    "X-CSRFToken": document.getElementsByName('csrfmiddlewaretoken')[0].value,
+                },
+                body: {
+                    text: this.input
+                }
+            };
+
+            cm.$http.post('/common/convert-markdown', form_data, options).then(
+                function (response) {
+                    this.compiledMarkdown = response.data;
+                }, function (response) {
+                    response.data;
+                });
+
+
+        },
+    },
+    methods: {
+        update: _.debounce(function (e) {
+            this.input = e.target.value
+        }, 300)
+    }
+});
